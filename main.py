dd775193eff89a0078eb15b699f38f995fa9eb7a
Darwin <jackdarwinbot@gmail.com>
2025-08-15 16:49:06 -0600

    Tercer Comit

--- main.py
+++ main.py
@@ -11,8 +11,34 @@ class RegistroProductos:
     def __init__(self):
         self.productos={}
     def agregar(self):
-        try:
-            codigo=input("Ingrese el Codigo del Producto: ")
+        while True:
+            codigo=input("Ingrese El Codigo del Producto:   ")
             if codigo in self.productos:
-                print("Este Codigo ya exste en el Sistema, intente uno nuevo")
-                return
+                print("Este Codigo Ya existe, Intentelo de nuevo...")
+            elif codigo=="":
+                print("El codigo no puede estar vacio, Intentelo de nuevo")
+            else:
+                break
+        nombre=input("Ingrese el Nombre del Producto:       ")
+        categoria=input("Ingrese La Categoria del Producto: ")
+        precio=float(input("Ingrese el Precio del Producto: "))
+        stock=int(input("Ingrese la cantidad en Stock:      "))
+        self.productos[codigo]=Producto(codigo,nombre,categoria,precio,stock)
+        print("El producto se Agrego Correctamente")
+
+registro=RegistroProductos()
+while True:
+    print("1. Agregar")
+    try:
+        opcion=int(input("seleccione una opcion"))
+        match opcion:
+            case 1:
+                registro.agregar()
+            case 2:
+                print("Salir")
+                break
+            case _:
+                print("Incorrecto")
+    except ValueError:
+        print("Ingrese un numero entero: ")
+
